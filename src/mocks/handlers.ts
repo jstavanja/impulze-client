1ca9a0395860f28234a1c104dfc8e72df65f90c2
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-12-30 12:59:45 +0100

    fix: :rotating_light: remove trailing semicolon from mock handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw'
 import mockUserInfo from './fixtures/user-info'
 import mockImpulzes from './fixtures/impulzes'
-import API_ROUTES from '../constants/api-routes';
+import API_ROUTES from '../constants/api-routes'
 
 export default [
   rest.get(API_ROUTES.USER.INFO, (req, res, ctx) => (res(
