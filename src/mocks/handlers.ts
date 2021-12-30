72a3946d97e6b42b7e6482bb189e873e171b4bfa
Jaka Stavanja <jaka.stavanja@oryxgaming.com>
2021-12-30 12:55:38 +0100

    refactor: :recycle: change backend route urls to match new ones

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,13 @@
 import { rest } from 'msw'
 import mockUserInfo from './fixtures/user-info'
 import mockImpulzes from './fixtures/impulzes'
+import API_ROUTES from '../constants/api-routes';
 
 export default [
-  rest.get('/api/auth/info', (req, res, ctx) => (res(
+  rest.get(API_ROUTES.USER.INFO, (req, res, ctx) => (res(
     ctx.json(mockUserInfo)
   ))),
-  rest.get('/api/impulze', (req, res, ctx) => (res(
+  rest.get(API_ROUTES.IMPULZE.INDEX, (req, res, ctx) => (res(
     ctx.json(mockImpulzes)
   )))
 ]
